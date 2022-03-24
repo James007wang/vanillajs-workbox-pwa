e6efedd98efb263a61e69d0ae8a2d1a8d95af1f8
James007wang <98909117+James007wang@users.noreply.github.com>
2022-03-24 07:22:48 -0400

    changed version of workbox

--- build/sw.js
+++ build/sw.js
@@ -1,4 +1,5 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+//ver 01
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 
 workbox.routing.registerRoute(
     new RegExp('https://jsonplaceholder.typicode.com/users'),
