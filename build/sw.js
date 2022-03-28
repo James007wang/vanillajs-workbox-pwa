6b320af064af2097ad540580a66ceac06b4aca40
James007wang <98909117+James007wang@users.noreply.github.com>
2022-03-28 15:16:24 -0400

    c

--- build/sw.js
+++ build/sw.js
@@ -1,6 +1,6 @@
 //ver 01
  importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
- console.log("workbox ready",workbox.strategies); 
+ console.log("workbox ready",workbox.strategies.CacheFirst); 
 
  workbox.precaching.precacheAndRoute([
   {
