b0153d22492e030ece472c36ab75328cdd446a9b
James007wang <98909117+James007wang@users.noreply.github.com>
2022-03-29 10:20:10 -0400

    c

--- build/sw.js
+++ build/sw.js
@@ -1,6 +1,8 @@
 //ver 01
  importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
- console.log("workbox ready",new workbox.strategies.CacheFirst); 
+ //console.log("workbox ready",new workbox.strategies.CacheFirst); 
+
+ console.log("workbox ready"); 
 
  /* workbox.routing.registerRoute(
   new RegExp('https://jsonplaceholder.typicode.com/users'),
