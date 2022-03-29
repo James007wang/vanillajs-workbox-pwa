1c3d93b2469f0ba6ba1896a8ed255a10e1975d66
James007wang <98909117+James007wang@users.noreply.github.com>
2022-03-29 10:23:05 -0400

    c

--- build/sw.js
+++ build/sw.js
@@ -1,5 +1,5 @@
 //ver 01
- importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+ //importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
  //console.log("workbox ready",new workbox.strategies.CacheFirst); 
 
  console.log("workbox ready"); 
